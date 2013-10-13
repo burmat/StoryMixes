fe12756f95e99ea86b56248b6bb4c322eb63c45f
Paul Melnikow <email@paulmelnikow.com>
2013-10-12 22:34:00 -0400

    Don't test in hackathon mode.

--- Gruntfile.js
+++ Gruntfile.js
@@ -559,12 +559,19 @@ module.exports = function ( grunt ) {
   /**
    * The `build` task gets your app ready to run for development and testing.
    */
+  // grunt.registerTask( 'build', [
+  //   'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'recess:build',
+  //   'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
+  //   'copy:build_appjs', 'copy:build_vendorjs', 'index:build', 'karmaconfig',
+  //   'karma:continuous' 
+  // ]);
+
   grunt.registerTask( 'build', [
     'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'recess:build',
     'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
-    'copy:build_appjs', 'copy:build_vendorjs', 'index:build', 'karmaconfig',
-    'karma:continuous' 
+    'copy:build_appjs', 'copy:build_vendorjs', 'index:build'
   ]);
+
 
   /**
    * The `compile` task gets your app ready for deployment by concatenating and
